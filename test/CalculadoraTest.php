5d44b93e0f9f80c9e5153975e4ea94008019e3c7
Nacho Calomarde <nachocalomarde@gmail.com>
2021-12-17 09:20:59 +0100

    Update CalculadoraTest.php

--- test/CalculadoraTest.php
+++ test/CalculadoraTest.php
@@ -4,7 +4,11 @@ include './src/Calculadora.php';
 
 final class CalculadoraTest extends TestCase
 {
-
+  public function testSuma()
+  {
+      $calc = new Calculadora(3,4);
+      $this->assertEquals(7,$calc->suma());
+  }
 }
 
 ?>
